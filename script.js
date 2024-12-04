9b1a58d3e35917c88f3b98fa87e7ff03c9c13fe0
BRUNO CARATI DA SILVA <bruno_carati-silva@estudante.sesisenai.org.br>
2024-12-03 22:15:19 -0300

    Adicionei algumas coisas de js e css

---
+++ script.js
@@ -0,0 +1,13 @@
+function cadastro(){
+    if(document.getElementById('cadastro').style.display == 'none'){
+
+        document.getElementById('cadastro').style.display = 'block'
+
+    }
+    else{
+
+        document.getElementById('cadastro').style.display = 'none'
+
+    } 
+    document.getElementById('cadastro').style.display = 'none'
+}                                   
